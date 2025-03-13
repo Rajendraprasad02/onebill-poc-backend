a9d8685021bda82227f263fbc99a6ba8bda8ee7c
Rajendraprasad02 <rajendra.prasad@talentakeaways.com>
2025-03-13 10:55:10 +0530

    error check - with upadted - 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -230,7 +230,8 @@ export class AppController {
       // Step 2: Use access token to fetch emails
       const mailsResponse = await axios.get(
         // 'https://graph.microsoft.com/v1.0/me/messages',
-        "https://graph.microsoft.com/v1.0/me/messages?$filter=messageType eq 'email'",
+        // "https://graph.microsoft.com/v1.0/me/messages?$filter=messageType eq 'email'",
+        'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages',
 
         {
           headers: { Authorization: `Bearer ${access_token}` },
