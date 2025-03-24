b7dd986273250a8a09ecf25c038be73e4fe95a10
Rajendraprasad02 <rajendra.prasad@talentakeaways.com>
2025-03-24 17:36:11 +0530

    bill details added - 4

--- src/modules/bill-details/bill-details.module.ts
+++ src/modules/bill-details/bill-details.module.ts
@@ -3,9 +3,10 @@ import { BillDetailsService } from './bill-details.service';
 import { BillDetailController } from './bill-details.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BillDetail } from './entities/bill-detail.entity';
+import { User } from 'modules/users/user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BillDetail])],
+  imports: [TypeOrmModule.forFeature([BillDetail, User])],
 
   controllers: [BillDetailController],
   providers: [BillDetailsService],
